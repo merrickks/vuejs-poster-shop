3927b443990e8945319abf901b986eae9d54ae48
Kendal Merrick <kendal@MacBook-Pro.localdomain>
2018-09-30 21:19:52 -0500

    First Commit - 00037

--- public/script.js
+++ public/script.js
@@ -1,1 +1,62 @@
-console.log('It works.');
+var PRICE = 9.99;
+
+new Vue({
+  el: "#app",
+  data: {
+    total: 0,
+    items: [],
+    cart: [],
+    newSearch: "",
+    lastSearch: ""
+  },
+  methods: {
+    onSubmit: function() {
+      this.$http.get("/search/".concat(this.newSearch)).then(function(res) {
+        console.log(res);
+        this.lastSearch = this.newSearch;
+        this.items = res.data;
+      });
+    },
+    addItem: function(index) {
+      this.total += PRICE;
+      var item = this.items[index];
+      var found = false;
+      for (var i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          found = true;
+          this.cart[i].qty++;
+          break;
+        }
+      }
+      if (!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          qty: 1,
+          price: PRICE
+        });
+      }
+    },
+    inc: function(item) {
+      item.qty++;
+      this.total += PRICE;
+    },
+    dec: function(item) {
+      item.qty--;
+      this.total -= PRICE;
+      if (item.qty <= 0) {
+        for (var i = 0; i < this.cart.length; i++) {
+          if (this.cart[i].id === item.id) {
+            this.cart.splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
+  },
+  filters: {
+    currency: function(price) {
+      return "$".concat(price.toFixed(2));
+    }
+  }
+});
